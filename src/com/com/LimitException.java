7fe19146b184f7011bc5274a61292ea45ff99ba0
AbdullazhanM <abulla_95@mail.ru>
2022-08-09 09:24:49 +0600

    Initial commit

---
+++ src/com/com/LimitException.java
@@ -0,0 +1,16 @@
+package com.com;
+
+public class LimitException extends Exception{
+    private double remainingAmount;
+
+    public LimitException(String message, double remainingAmount) {
+        super(message);
+        this.remainingAmount = remainingAmount;
+    }
+
+    public double getRemainingAmount() {
+        return remainingAmount;
+    }
+
+
+}
